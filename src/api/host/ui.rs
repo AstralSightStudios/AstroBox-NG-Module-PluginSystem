c6f0e4922ce37317529a28a178bfc270d863c7ec
Searchstars <searchstars@163.com>
2025-12-06 21:19:29 +0800

    fmt

--- src/api/host/ui.rs
+++ src/api/host/ui.rs
@@ -1,7 +1,7 @@
 use std::collections::HashMap;
 
-use rand::distr::Alphanumeric;
 use rand::Rng;
+use rand::distr::Alphanumeric;
 use serde::Serialize;
 use tauri::Emitter;
 use wasmtime::component::Resource;
