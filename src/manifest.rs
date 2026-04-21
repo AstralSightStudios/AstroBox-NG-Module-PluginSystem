95c67e3037c7ff2b040b6fe1857ed8ed8c76d3a4
Searchstars <searchstars@163.com>
2026-04-21 23:04:08 +0800

    加参，，

--- src/manifest.rs
+++ src/manifest.rs
@@ -18,6 +18,12 @@ pub struct PluginManifest {
     pub permissions: Vec<String>, // 插件权限列表
     #[serde(default)]
     pub additional_files: Vec<String>, // 插件附加文件列表
+    #[serde(
+        default,
+        rename = "enableSettingsButton",
+        skip_serializing_if = "Option::is_none"
+    )]
+    pub enable_settings_button: Option<bool>, // 是否在插件窗口右上角显示设置按钮
 }
 
 impl PluginManifest {
